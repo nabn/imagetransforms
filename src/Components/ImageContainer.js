a6fbf20ba4f6f30df02396b32b6664d480c2fd91
nabn <nabeen.khadka@gmail.com>
2017-02-27 11:56:08 +0545

    Add ease transition and a default background if no image

--- src/Components/ImageContainer.js
+++ src/Components/ImageContainer.js
@@ -2,12 +2,17 @@ import React from 'react'
 
 const ImageContainer = ({ handleFileChange, src, transforms, classNameSuffixes }) => (
   <div className='image-container'>
-    <div className='image-wrapper'>
-      <img
-        src={src}
-        role='presentation'
-        style={{ transform: transforms.join(' ') }}
-        className={'picked-image ' + classNameSuffixes.join(' ')}/>
+    <div
+      className='image-wrapper'
+      style={!src ? {background: '#eee'} : {}}>
+      {src
+        ? <img
+            src={src}
+            role='presentation'
+            style={{ transform: transforms.join(' ') }}
+            className={'picked-image ' + classNameSuffixes.join(' ')}/>
+        : 'Please pick an image first'
+      }
     </div>
     <input
       onChange={handleFileChange}
